82caed2c24b0642dd98cf0004b04443edb288164
Sherwin Wu <thesherwinwu@gmail.com>
2013-12-03 14:13:11 -0500

    deleted gallery

--- moocenimages/app/controllers/visualizations_controller.rb
+++ moocenimages/app/controllers/visualizations_controller.rb
@@ -1,6 +1,21 @@
 class VisualizationsController < ApplicationController
 
+  def home
+
+  end
+
   def index
-    
+    @visualizations = Visualization.find(:all)
+  end
+
+  def get_file
+    name = params[:name]
+    viz = Visualization.find(:id => 5)
+    @file = viz.content
+
+    respond_to do |format|
+        format.json { render json: @file }
+      end
+    end
   end
 end
--- moocenimages/config/routes.rb
+++ moocenimages/config/routes.rb
@@ -1,11 +1,10 @@
 Moocenimages::Application.routes.draw do
-  get "gallery" => 'gallery#index'
   root :to => "visualizations#index"
-
 
   resources :users
   resources :user_sessions
 
   get 'login' => 'user_sessions#new', :as => :login
   get 'logout' => 'user_sessions#destroy', :as => :logout
+
 end
